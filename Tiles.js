eca16380b8d6874b2dbfab8331532af81c82a1c7
Anish Narang <anish_an@hotmail.com>
2015-09-29 22:47:43 +0530

    Added file save option

--- Tiles.js
+++ Tiles.js
@@ -198,3 +198,36 @@ function TileAction()
 		$( ".tiles" ).disableSelection();
 	};
 }
+
+function saveTextAsFile()
+{
+	var textToWrite = editor.getContent();
+	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+	var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+
+	var downloadLink = document.createElement("a");
+	downloadLink.download = fileNameToSaveAs;
+	downloadLink.innerHTML = "Download File";
+	if (window.webkitURL != null)
+	{
+		// Chrome allows the link to be clicked
+		// without actually adding it to the DOM.
+		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+	}
+	else
+	{
+		// Firefox requires the link to be added to the DOM
+		// before it can be clicked.
+		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+		downloadLink.onclick = destroyClickedElement;
+		downloadLink.style.display = "none";
+		document.body.appendChild(downloadLink);
+	}
+
+	downloadLink.click();
+	document.getElementById("inputFileNameToSaveAs").value = "";
+}
+function destroyClickedElement(event)
+{
+	document.body.removeChild(event.target);
+}
