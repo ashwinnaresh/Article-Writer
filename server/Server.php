b974aa3a814b553b9d59d282c8c65ef7bfad6301
madhuradas <das.madhura94@gmail.com>
2015-10-02 12:02:51 +0530

    Added concept call

--- server/Server.php
+++ server/Server.php
@@ -6,15 +6,14 @@ $alchemyapi = new AlchemyAPI();
 // $textapi = new AYLIEN\TextAPI("a6379b0a", "6f2cf6012da38962a60079bb4b7cea9c",false);
 // Keyword extraction
 extract($_GET);
-$threshold = 0;
+$res_arr=array();
+$thresh_text = 50;
 $keywords = array();
-$concepts = array();
 $text = rawurldecode($demo_text);
-$count = count($text);
-if($count<=50)
-{
-	$text = "Machine learning is a subfield of computer science that evolved from the study of pattern recognition and computational learning theory in artificial intelligence. Machine learning explores the study and construction of algorithms that can learn from and make predictions on data.";
-// $response = $alchemyapi->keywords('text',$text, array('maxRetrieve'=>5));
+$text = "Machine learning is a subfield of computer science that evolved from the study of pattern recognition and computational learning theory in artificial intelligence. Machine learning explores the study and construction of algorithms that can learn from and make predictions on data.";
+$text_arr = explode(" ",$text);
+$word_count = count($text_arr);
+// $response = $alchemyapi->keywords('text',$text, array('maxRetrieve'=>2));
 // if ($response['status'] == 'OK') 
 // {
 // 	$words = '';
@@ -27,39 +26,17 @@ if($count<=50)
 // 			$keywords[$keyword['text']]=$keyword['relevance'];
 // 		}
 // 	}
-	$keywords = array('computational learning theory'=> '0.971245',
-					'pattern recognition'=> '0.854866',
-					'artificial intelligence'=> '0.793011',
-					'Machine learning' => '0.714784',
-					'subfield'=> '0.638192');
-	 arsort($keywords);
-
 // } 
 // else 
 // {
 // 	echo 'Error in the keyword extraction call: ', $response['statusInfo'];
 // }
-
-// $response = $alchemyapi->concepts('text',rawurldecode($demo_text), null);
-// if ($response['status'] == 'OK') 
-// {
-// 	$con = '';
-// 	foreach ($response['concepts'] as $concept) 
-// 	{
-// 		// $con=$con . ';' . $concept['text'] . ':' . $concept['relevance'];
-// 		if($concept['relevance'] >= $threshold)
-// 		{
-// 			array_push($concepts, $concept['text']);
-// 			$con = $con . ';' . $concept['text'] . ':' . $concept['relevance'];
-// 		}
-// 	}
-// 	// echo "CONCEPT = " . $con;
-// } 
-// else 
-// {
-// 	echo 'Error in the concept tagging call: ', $response['statusInfo'];
-// }
-
+$keywords = array('computational learning theory'=> '0.971245',
+					'pattern recognition'=> '0.854866',
+					'artificial intelligence'=> '0.793011',
+					'Machine learning' => '0.714784',
+					'subfield'=> '0.638192');
+	 arsort($keywords);
 // Algorithm for picking top keywords and concepts and performing a search 
 // For 50 words or lesser, perform a search based on the keywords, otherwise, search based on words
 // common to keywords and concepts (? might need to be changed)
@@ -68,9 +45,7 @@ if($count<=50)
 	$rootUri = 'https://api.datamarket.azure.com/Bing/Search';
 	$market = urlencode("'en-us'");
 	$serviceOp = 'Web';
-	$limit = 2;
-	$chk_limit = 0;
-	$res_arr=array();
+	$limit = 0;
 	//search only keywords
 	foreach ($keywords as $word => $relevance) 
 	{
@@ -120,12 +95,78 @@ if($count<=50)
 	$res_arr[] = array('search_term' => $word, 'results' => $res_inner_arr);
 	}
 
-	echo json_encode($res_arr);//$res_arr;
-}
-
-else
+if($word_count>=(int)$thresh_text)
 {
-	//both keywords + concepts search
+	
+//    $response = $alchemyapi->concepts('text',$text,array('maxRetrieve'=>2));
+//  if ($response['status'] == 'OK') 
+//  {
+// 	$con = array();
+// 	foreach ($response['concepts'] as $concept) 
+// 	{
+// 			//array_push($concepts, $concept['text']);
+// 			$con[$concept['text']] = $concept['relevance'];
+// 	}
+// } 
+// else 
+// {
+// 	echo 'Error in the concept tagging call: ', $response['statusInfo'];
+// }
+$con = array('computational learning theory'=> '0.971245',
+					'Computer'=> '0.854866',
+					'artificial intelligence'=> '0.793011',
+					'Machine learning' => '0.714784',
+					'subfield'=> '0.638192');
+arsort($con);
+	foreach ($con as $word => $relevance) 
+	{
+	if(!array_key_exists($word, $keywords))
+	{
+	//Search for each keyword
+	$keyword = "" . $word;
+	$query = rawurlencode("'$keyword'");
+	$requestUri = "$rootUri/$serviceOp?\$format=json&Query=$query&Market=$market";
+	// Encode the credentials and create the stream context.
+	$auth = base64_encode("$acctKey:$acctKey");
+	$data = array(
+					'http' => array(
+									'request_fulluri' => true,
+									// ignore_errors can help debug – remove for production. This option added in PHP 5.2.10
+									'ignore_errors' => true,
+									'header' => "Authorization: Basic $auth"
+									)
+				);
+	$context = stream_context_create($data);
+	// Get the response from Bing.
+	$response = file_get_contents($requestUri, 0, $context);
+	//echo gettype($response);
+	$arr=json_decode($response,true);
+	$res_inner_arr=array();
+	$search_res = $arr['d']['results'];
+	//Create json for each search result
+	for($i=0;$i<5;$i++)
+		{
+			foreach ($search_res[$i] as $inner => $v) 
+			{
+				if($inner == 'Url')
+				{
+					//Dummy text as AYLIEN causing problems
+					$text_data="'$limit'";
+					$limit=$limit+1;
+					//$summary = $textapi->Summarize(array('url' => $v, 'sentences_number' => 5));
+					// foreach ($summary->sentences as $sentece) {
+    						// $text_data=$text_data.' '.$sentece;
+    					// }
+    				if(strlen($text_data)>0)
+    				{
+     					$res_inner_arr[] = array('Description' => $text_data,'Url' => $v );
+     				}
+    			}	
+			}	
+		}
+		$res_arr[] = array('search_term' => $word, 'results' => $res_inner_arr);
+	}
 }
-
+}
+echo json_encode($res_arr);
 ?>
