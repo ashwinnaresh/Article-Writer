673a1ad2e47565c5aeae1c8040f38a4717622689
madhuradas <das.madhura94@gmail.com>
2015-10-15 09:37:22 +0530

    Added searchServer

--- server/Server.php
+++ server/Server.php
@@ -10,8 +10,14 @@ $res_arr = array();
 $more_res = array();
 $keywords = array();
 $text = rawurldecode($demo_text);
-//$already_searched = fopen("search_terms.txt","r");
-
+$file_searched = fopen("search_terms.txt","a+");
+$size = filesize("search_terms.txt");
+if($size == 0)
+	$searched = array();
+else{
+$searched = fread($file_searched,filesize("search_terms.txt"));
+$searched = explode(";", $searched);
+}
 // $text = "Machine learning is a subfield of computer science that evolved from the study of pattern recognition and computational learning theory in artificial intelligence. Machine learning explores the study and construction of algorithms that can learn from and make predictions on data.";
 
 // $response = $alchemyapi->keywords('text',$text, array('maxRetrieve'=>2));
@@ -50,61 +56,67 @@ $limit = 0;
 //search only keywords
 foreach ($keywords as $word => $relevance) 
 {
-	//Search for each keyword
-	$keyword = "" . $word;
-	$query = rawurlencode("'$keyword'");
-	$requestUri = "$rootUri/$serviceOp?\$format=json&Query=$query&Market=$market";
-	// Encode the credentials and create the stream context.
-	$auth = base64_encode("$acctKey:$acctKey");
-	$data = array(
-					'http' => array(
-									'request_fulluri' => true,
-									// ignore_errors can help debug – remove for production. This option added in PHP 5.2.10
-									'ignore_errors' => true,
-									'header' => "Authorization: Basic $auth"
-									)
-				);
-	$context = stream_context_create($data);
-	// Get the response from Bing.
-	$response = file_get_contents($requestUri, 0, $context);
-	//echo gettype($response);
-	$arr=json_decode($response,true);
-	$res_inner_arr=array();
-	$search_res = $arr['d']['results'];
-	// array_push($res_arr,$search_res);
-	//Create json for each search result
-	for($i=0;$i<5;$i++)
+	if(!in_array($word, $searched))
 	{
-		foreach ($search_res[$i] as $inner => $v) 
+		echo "here $word<br>";
+		//Search for each keyword
+		$keyword = "" . $word;
+		fwrite($file_searched, $word.";");
+		$query = rawurlencode("'$keyword'");
+		$requestUri = "$rootUri/$serviceOp?\$format=json&Query=$query&Market=$market";
+		// Encode the credentials and create the stream context.
+		$auth = base64_encode("$acctKey:$acctKey");
+		$data = array(
+						'http' => array(
+										'request_fulluri' => true,
+										// ignore_errors can help debug – remove for production. This option added in PHP 5.2.10
+										'ignore_errors' => true,
+										'header' => "Authorization: Basic $auth"
+										)
+					);
+		$context = stream_context_create($data);
+		// Get the response from Bing.
+		$response = file_get_contents($requestUri, 0, $context);
+		//echo gettype($response);
+		$arr=json_decode($response,true);
+		$res_inner_arr=array();
+		$search_res = $arr['d']['results'];
+		// array_push($res_arr,$search_res);
+		//Create json for each search result
+		for($i=0;$i<5;$i++)
 		{
-			if($inner == 'Url')
+			foreach ($search_res[$i] as $inner => $v) 
 			{
-				$res_inner_arr[] = array('Description' => $search_res[$i]['Description'],'Url' => $v );
+				if($inner == 'Url')
+				{
+					$res_inner_arr[] = array('Description' => $search_res[$i]['Description'],'Url' => $v );
+				}	
 			}	
-		}	
-	}
-	$more_res_inner_arr = array();
-	for($i=5;$i<10;$i++)
-	{
-		foreach ($search_res[$i] as $inner => $v) 
+		}
+		$more_res_inner_arr = array();
+		for($i=5;$i<10;$i++)
 		{
-			if($inner == 'Url')
+			foreach ($search_res[$i] as $inner => $v) 
 			{
-				$more_res_inner_arr[] = array('Description' => $search_res[$i]['Description'],'Url' => $v );
+				if($inner == 'Url')
+				{
+					$more_res_inner_arr[] = array('Description' => $search_res[$i]['Description'],'Url' => $v );
+				}	
 			}	
-		}	
+		}
+		//Final results
+		$more_res[] = array('search_term' => $word, 'results' => $more_res_inner_arr);
+		$res_arr[] = array('search_term' => $word, 'results' => $res_inner_arr);
 	}
-	//Final results
-	$more_res[] = array('search_term' => $word, 'results' => $more_res_inner_arr);
-	$res_arr[] = array('search_term' => $word, 'results' => $res_inner_arr);
 }
 
 if($concept != "empty")
 {
-	$concept ="Machine Learning";
-	if(!array_key_exists($concept, $keywords))
+	$concept ="Machine learning";
+	if(!array_key_exists($concept, $keywords) && !in_array($concept, $searched))
 	{
 		//Search for each keyword
+		echo "here $concept<br>";
 		$keyword = "" . $concept;
 		$query = rawurlencode("'$keyword'");
 		$requestUri = "$rootUri/$serviceOp?\$format=json&Query=$query&Market=$market";
@@ -148,11 +160,13 @@ if($concept != "empty")
 			}	
 		}
 		//Final results
+		fwrite($file_searched,$concept.";");
 		$more_res[] = array('search_term' => $concept, 'results' => $more_res_inner_arr);
 		$res_arr[] = array('search_term' => $concept, 'results' => $res_inner_arr);
 	}
 
 }
+fclose($file_searched);
 $file = fopen("more_results.json","w");
 fwrite($file,json_encode($more_res));
 fclose($file);
